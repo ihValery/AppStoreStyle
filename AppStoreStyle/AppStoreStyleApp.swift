76bd1c2479f2690eff106e3a4400f2737c068cd8
ihValery <ihValery@gmail.com>
2021-08-12 05:48:37 +0300

    Initial Commit

---
+++ AppStoreStyle/AppStoreStyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreStyleApp.swift
+//  AppStoreStyle
+//
+//  Created by Валерий Игнатьев on 12.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreStyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
